da838641c6fef9e52e8ff9357786995bd1576ced
unknown <scott73carlson@gmail.com>
2023-02-28 20:57:21 -0600

    created Author and Category model files

---
+++ models/Author.php
@@ -0,0 +1,67 @@
+<?php
+    class Author {
+        //DB stuff
+        private $conn;
+        private $table = 'authors';
+
+        //Post Properties
+        public $id;
+        public $author;
+
+        //Constructor with DB
+
+        public function __construct($db) {
+            $this->conn = $db;
+        }
+
+        //Get Authors
+        public function read() {
+            //Create query
+            $query = "SELECT 
+            id, author
+            FROM 
+                $this->table
+            ORDER BY id";
+
+        //Prepared statements
+        $stmt = $this->conn->prepare($query);
+
+        //Execute query
+        $stmt->execute();
+
+        return $stmt;
+            
+        }
+
+        //Get single Author
+
+        public function read_single() {
+             //Create query
+             $query = "SELECT 
+             author
+             FROM 
+                $this->table
+             WHERE
+             id = ?";
+
+        //Prepared statements
+        $stmt = $this->conn->prepare($query);
+
+        //Bind ID
+        $stmt->bindParam(1, $this->id);
+
+        //Execute query
+        $stmt->execute();
+
+        $row = $stmt->fetch(PDO::FETCH_ASSOC);
+
+        //Set properties
+        $this-id = $row['id'];
+        $this-author = $row['author'];
+        
+
+        }
+    }
+
+
+?>
---
+++ models/Category.php
@@ -0,0 +1,67 @@
+<?php
+    class Category {
+        //DB stuff
+        private $conn;
+        private $table = 'categories';
+
+        //Post Properties
+        public $id;
+        public $author;
+
+        //Constructor with DB
+
+        public function __construct($db) {
+            $this->conn = $db;
+        }
+
+        //Get Categories
+        public function read() {
+            //Create query
+            $query = "SELECT 
+            id, category
+            FROM 
+                $this->table
+            ORDER BY id";
+
+        //Prepared statements
+        $stmt = $this->conn->prepare($query);
+
+        //Execute query
+        $stmt->execute();
+
+        return $stmt;
+            
+        }
+
+        //Get single Category
+
+        public function read_single() {
+             //Create query
+             $query = "SELECT 
+             category
+             FROM 
+                $this->table
+             WHERE
+             id = ?";
+
+        //Prepared statements
+        $stmt = $this->conn->prepare($query);
+
+        //Bind ID
+        $stmt->bindParam(1, $this->id);
+
+        //Execute query
+        $stmt->execute();
+
+        $row = $stmt->fetch(PDO::FETCH_ASSOC);
+
+        //Set properties
+        $this-id = $row['id'];
+        $this-author = $row['category'];
+        
+
+        }
+    }
+
+
+?>
